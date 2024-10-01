85ae91420771c499ba16c24a9771785c1d008d72
Ann@ <ann-brown956@yandex.ru>
2024-10-01 15:12:45 +0300

    выгрузила проект в гит хаб

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  Tracker
-//
-//  Created by Анна Браун on 01.10.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
